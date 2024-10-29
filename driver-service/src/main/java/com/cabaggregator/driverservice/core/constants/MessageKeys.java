4ddf17ea6951ec745f3fd0d906e91c747b8c2c5d
danilalisichkin <danilafox3467346@gmail.com>
2024-10-29 20:20:58 +0300

    feat: add messages for validation of car details responses

--- driver-service/src/main/java/com/cabaggregator/driverservice/core/constants/MessageKeys.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/core/constants/MessageKeys.java
@@ -13,6 +13,7 @@ public abstract  class MessageKeys {
 
     public static final String ERROR_INVALID_SORT_ORDER = "error.invalid.sort.order";
     public static final String ERROR_INVALID_PHONE_FORMAT = "error.invalid.phone.format";
+    public static final String ERROR_INVALID_LICENCE_PLATE_FORMAT = "error.invalid.licence.plate.format";
     public static final String ERROR_INVALID_LENGTH = "error.invalid.length";
 
     public static final String ERROR_DRIVERS_NOT_FOUND = "error.drivers.not.found";
@@ -26,7 +27,9 @@ public abstract  class MessageKeys {
     public static final String ERROR_CAR_WITH_LICENCE_PLATE_NOT_FOUND="error.car.with.license.plate.not.found";
     public static final String ERROR_CAR_WITH_LICENCE_PLATE_ALREADY_EXISTS="error.car.with.license.plate.already.exists";
 
+    public static final String ERROR_CAR_DETAILS_WITH_ID_NOT_FOUND="error.car.details.with.id.not.found";
     public static final String ERROR_CAR_DETAILS_WITH_CAR_ID_NOT_FOUND="error.car.details.with.car.id.not.found";
+    public static final String ERROR_CAR_DETAILS_WITH_CAR_ID_ALREADY_EXISTS="error.car.details.with.car.id.already.exists";
 
     public static final String ERROR_CANT_READ_REQUEST="error.cant.read.request";
     public static final String ERROR_IF_PERSISTS_CONTACT_DEVELOPERS="error.if.persists.contact.developers";
