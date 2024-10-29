a4b5fd34ee7af2cb14f0bdc6f9ead8371e8af8b2
danilalisichkin <danilafox3467346@gmail.com>
2024-10-29 17:34:05 +0300

    refactor: change fields of CarAddingDTO

--- driver-service/src/main/java/com/cabaggregator/driverservice/core/dto/CarAddingDTO.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/core/dto/CarAddingDTO.java
@@ -1,7 +1,13 @@
 package com.cabaggregator.driverservice.core.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CarAddingDTO extends CarDTO {
-    public CarAddingDTO(long id, String licensePlate, String make, String model, String color) {
-        super(id, licensePlate, make, model, color);
-    }
+    String licensePlate;
+    String make;
+    String model;
+    String color;
 }
