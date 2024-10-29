6dbe75ef21f0623df028df3288b4871a230005b0
danilalisichkin <danilafox3467346@gmail.com>
2024-10-29 18:22:50 +0300

    feat: add needed methods for DriverRepository

--- driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/DriverRepository.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/DriverRepository.java
@@ -3,5 +3,14 @@ package com.cabaggregator.driverservice.dao.repository;
 import com.cabaggregator.driverservice.entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+    Optional<Driver> findByPhoneNumber(String phoneNumber);
+
+    Optional<Driver> findByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    boolean existsByEmail(String email);
 }
