b992a9b40021047d9f6f5c783efbfa19d7ce4a2a
danilalisichkin <danilafox3467346@gmail.com>
2024-10-29 18:22:41 +0300

    feat: add needed methods for CarDetailsRepository

--- driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/CarDetailsRepository.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/CarDetailsRepository.java
@@ -4,6 +4,11 @@ import com.cabaggregator.driverservice.entities.CarDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CarDetailsRepository extends JpaRepository<CarDetails, Long> {
+    Optional<CarDetails> findByCarId(long carId);
+
+    boolean existsByCarId(long carId);
 }
