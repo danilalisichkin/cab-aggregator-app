bfbb70d66db19edfbbb7ece31497ce5a6577bb75
danilalisichkin <danilafox3467346@gmail.com>
2024-10-29 19:26:12 +0300

    fix: add @Repository annotation for DriverRepository

--- driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/DriverRepository.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/dao/repository/DriverRepository.java
@@ -2,9 +2,11 @@ package com.cabaggregator.driverservice.dao.repository;
 
 import com.cabaggregator.driverservice.entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
     Optional<Driver> findByPhoneNumber(String phoneNumber);
 
