da3797f2680785f577af160c4615bfda686fa5f5
danilalisichkin <danilafox3467346@gmail.com>
2024-11-12 22:59:17 +0300

    refactor(driver-service): refactor code
    - changed MultiErrorResponse DTO
    - added exception handler for validation exceptions
    - refactor validation error adding

--- driver-service/src/main/java/com/cabaggregator/driverservice/exception/RestExceptionHandler.java
+++ driver-service/src/main/java/com/cabaggregator/driverservice/exception/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import com.cabaggregator.driverservice.core.dto.error.ErrorResponse;
 import com.cabaggregator.driverservice.core.dto.error.MultiErrorResponse;
 import com.cabaggregator.driverservice.util.MessageBuilder;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -17,8 +18,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 @Slf4j
 @RestControllerAdvice
@@ -32,7 +36,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(
                         e.getLocalizedMessage(),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_NOT_FOUND, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.NOT_FOUND, null)));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -41,7 +45,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(
                         messageBuilder.buildLocalizedMessage(e.getMessageKey(), e.getMessageArgs()),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_NOT_FOUND, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.NOT_FOUND, null)));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
@@ -50,7 +54,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(
                         e.getMessage(),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_BAD_REQUEST, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.BAD_REQUEST, null)));
     }
 
     @ExceptionHandler(BadRequestException.class)
@@ -59,7 +63,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(
                         messageBuilder.buildLocalizedMessage(e.getMessageKey(), e.getMessageArgs()),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_BAD_REQUEST, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.BAD_REQUEST, null)));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
@@ -67,20 +71,20 @@ public class RestExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CANT_READ_REQUEST, null),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_BAD_REQUEST, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.CANT_READ_REQUEST, null),
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.BAD_REQUEST, null)));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleNoValidException(MethodArgumentNotValidException e) {
-        Map<String, String> errorMap = new HashMap<>();
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<MultiErrorResponse> handleNoValidException(MethodArgumentNotValidException e) {
+        Map<String, List<String>> errorMap = new HashMap<>();
 
         getValidationErrors(errorMap, e);
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new MultiErrorResponse(
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_VALIDATION, null),
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.VALIDATION, null),
                         errorMap));
     }
 
@@ -90,7 +94,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body(new ErrorResponse(
                         e.getMessage(),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_UNIQUENESS_CONFLICT, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.UNIQUENESS_CONFLICT, null)));
     }
 
     @ExceptionHandler(DataUniquenessConflictException.class)
@@ -99,7 +103,7 @@ public class RestExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body(new ErrorResponse(
                         messageBuilder.buildLocalizedMessage(e.getMessageKey(), e.getMessageArgs()),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_UNIQUENESS_CONFLICT, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.UNIQUENESS_CONFLICT, null)));
     }
 
     @ExceptionHandler(Exception.class)
@@ -109,15 +113,22 @@ public class RestExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse(
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_IF_PERSISTS_CONTACT_DEVELOPERS, null),
-                        messageBuilder.buildLocalizedMessage(MessageKeys.ERROR_CAUSE_INTERNAL, null)));
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.CONTACT_DEVELOPERS, null),
+                        messageBuilder.buildLocalizedMessage(MessageKeys.ErrorCauses.INTERNAL, null)));
     }
 
-    private void getValidationErrors(Map<String, String> errorMap, MethodArgumentNotValidException e) {
-        e.getBindingResult().getFieldErrors().forEach(error -> {
-            String fieldName = error.getField();
-            String errorMessage = error.getDefaultMessage();
-            errorMap.put(fieldName, errorMessage);
-        });
+    private void getValidationErrors(Map<String, List<String>> errorMap, Exception e) {
+        BiConsumer<String, String> addError = (field, message) ->
+                errorMap.computeIfAbsent(field, k -> new ArrayList<>()).add(message);
+
+        if (e instanceof MethodArgumentNotValidException validationEx) {
+            validationEx.getBindingResult()
+                    .getFieldErrors().forEach(error ->
+                            addError.accept(error.getField(), error.getDefaultMessage()));
+        } else if (e instanceof ConstraintViolationException constraintEx) {
+            constraintEx.getConstraintViolations()
+                    .forEach(violation ->
+                            addError.accept(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
     }
 }
