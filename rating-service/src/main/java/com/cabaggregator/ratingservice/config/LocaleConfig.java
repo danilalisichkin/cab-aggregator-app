2ca2ade5ade25953c4fc24ce1f75ccb624ff1b4d
danilalisichkin <danilafox3467346@gmail.com>
2024-11-25 16:23:46 +0300

    fix(rating-service): fix flaws
    - added last @FieldDefaults

--- rating-service/src/main/java/com/cabaggregator/ratingservice/config/LocaleConfig.java
+++ rating-service/src/main/java/com/cabaggregator/ratingservice/config/LocaleConfig.java
@@ -1,6 +1,8 @@
 package com.cabaggregator.ratingservice.config;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,9 @@ import java.util.Locale;
 
 @Configuration
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LocaleConfig implements WebMvcConfigurer {
-    private final MessageConfig messageConfig;
+    MessageConfig messageConfig;
 
     @Bean
     public MessageSource messageSource() {
