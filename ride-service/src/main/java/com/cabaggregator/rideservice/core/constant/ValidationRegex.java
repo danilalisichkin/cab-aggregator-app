5b9c0f04f841fa38c238d33e8a09b5794ca503f8
danilalisichkin <danilafox3467346@gmail.com>
2024-12-04 16:30:09 +0300

    chore(ride-service): remove deprecated version of service

--- ride-service/src/main/java/com/cabaggregator/rideservice/core/constant/ValidationRegex.java
+++
@@ -1,8 +0,0 @@
-package com.cabaggregator.rideservice.core.constant;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class ValidationRegex {
-    public static final String SORT_ORDER = "(?i)^(asc|desc)$";
-}
