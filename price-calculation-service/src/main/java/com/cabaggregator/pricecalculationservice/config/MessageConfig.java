61fcbe745d6b8daa0473a867a6b8b64a50a43482
danilalisichkin <danilafox3467346@gmail.com>
2024-12-06 12:40:26 +0300

    feat(price-calculation-service): add exceptions & localization

---
+++ price-calculation-service/src/main/java/com/cabaggregator/pricecalculationservice/config/MessageConfig.java
@@ -0,0 +1,13 @@
+package com.cabaggregator.pricecalculationservice.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "spring.messages.basename")
+public class MessageConfig {
+    private final String errorCauses;
+    private final String encoding;
+}
