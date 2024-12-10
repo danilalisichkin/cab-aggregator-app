b0f3878a5e79e242ea37bb1f00b9866a5a5ee971
Danila Lisichkin <113123614+danilalisichkin@users.noreply.github.com>
2024-12-10 12:56:49 +0300

    feat(rating-service): refactor tests (#51)

--- rating-service/src/test/java/com/cabaggregator/ratingservice/config/AbstractIntegrationTest.java
+++ rating-service/src/test/java/com/cabaggregator/ratingservice/config/AbstractIntegrationTest.java
@@ -3,9 +3,11 @@ package com.cabaggregator.ratingservice.config;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 
 @ActiveProfiles("test")
+@TestPropertySource(properties = {"mongock.enabled=false"})
 public abstract class AbstractIntegrationTest {
     static final MongoDBContainer mongoDBContainer =
             new MongoDBContainer("mongo:7.0").withExposedPorts(27017);
