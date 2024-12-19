b78c13dd8d98f3ea48036277db077f1d51454476
danilalisichkin <danilafox3467346@gmail.com>
2024-12-19 13:09:06 +0300

    feat(rating-service): add API docs

--- rating-service/src/main/java/com/cabaggregator/ratingservice/core/dto/passenger/PassengerRateAddingDto.java
+++ rating-service/src/main/java/com/cabaggregator/ratingservice/core/dto/passenger/PassengerRateAddingDto.java
@@ -1,10 +1,12 @@
 package com.cabaggregator.ratingservice.core.dto.passenger;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import org.bson.types.ObjectId;
 
 import java.util.UUID;
 
+@Schema(description = "Entry to add new passenger rate")
 public record PassengerRateAddingDto(
         @NotNull
         ObjectId rideId,
