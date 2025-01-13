e3b41f50a79fa2e7a27a702f021611393f392639
danilalisichkin <danilafox3467346@gmail.com>
2025-01-13 16:58:41 +0300

    refactor(ride-service): refactor API

--- ride-service/src/main/java/com/cabaggregator/rideservice/controller/api/RideDriverController.java
+++ ride-service/src/main/java/com/cabaggregator/rideservice/controller/api/RideDriverController.java
@@ -2,14 +2,14 @@ package com.cabaggregator.rideservice.controller.api;
 
 import com.cabaggregator.rideservice.core.constant.ValidationErrors;
 import com.cabaggregator.rideservice.core.dto.page.PageDto;
-import com.cabaggregator.rideservice.core.dto.ride.RideAddingDto;
 import com.cabaggregator.rideservice.core.dto.ride.RideDto;
+import com.cabaggregator.rideservice.core.enums.PaymentStatus;
 import com.cabaggregator.rideservice.core.enums.RideStatus;
 import com.cabaggregator.rideservice.core.enums.sort.RideSortField;
+import com.cabaggregator.rideservice.service.RideDriverService;
 import com.cabaggregator.rideservice.service.RidePaymentService;
-import com.cabaggregator.rideservice.service.RideService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
@@ -19,26 +19,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/rides")
-public class RideController {
+@RequestMapping("api/v1/drivers")
+public class RideDriverController {
 
-    private final RideService rideService;
+    private final RideDriverService rideDriverService;
 
     private final RidePaymentService ridePaymentService;
 
-    @GetMapping
-    public ResponseEntity<PageDto<RideDto>> getPageOfRides(
+    @GetMapping("/{driverId}/rides")
+    public ResponseEntity<PageDto<RideDto>> getPageOfDriverRides(
+            @PathVariable UUID driverId,
             @RequestParam(defaultValue = "0")
             @PositiveOrZero(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE_OR_ZERO) Integer offset,
             @RequestParam(defaultValue = "10")
@@ -48,47 +50,40 @@ public class RideController {
             @RequestParam(defaultValue = "ASC") Sort.Direction sortOrder,
             @RequestParam(required = false) RideStatus status) {
 
-        PageDto<RideDto> rides = rideService.getPageOfRides(offset, limit, sortBy, sortOrder, status);
+        PageDto<RideDto> driverRides =
+                rideDriverService.getPageOfDriverRides(driverId, offset, limit, sortBy, sortOrder, status);
 
-        return ResponseEntity.status(HttpStatus.OK).body(rides);
+        return ResponseEntity.status(HttpStatus.OK).body(driverRides);
     }
 
-    @GetMapping("/requests")
-    public ResponseEntity<PageDto<RideDto>> getRequestedRides(
-            @RequestParam(defaultValue = "0")
-            @PositiveOrZero(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE_OR_ZERO) Integer offset,
-            @RequestParam(defaultValue = "10")
-            @Positive(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE)
-            @Max(value = 40, message = ValidationErrors.INVALID_NUMBER_MAX_VALUE) Integer limit,
-            @RequestParam(defaultValue = "id") RideSortField sortBy,
-            @RequestParam(defaultValue = "ASC") Sort.Direction sortOrder) {
-
-        PageDto<RideDto> availableRides =
-                rideService.getPageOfRequestedRides(offset, limit, sortBy, sortOrder);
-
-        return ResponseEntity.status(HttpStatus.OK).body(availableRides);
-    }
-
-    @PutMapping("/rides/requests/{id}")
-    public ResponseEntity<RideDto> acceptRide(
+    @GetMapping("/{driverId}/rides/{id}")
+    public ResponseEntity<RideDto> getRide(
+            @PathVariable UUID driverId,
             @PathVariable ObjectId id) {
-
-        RideDto acceptedRide = rideService.acceptRide(id);
-
-        return ResponseEntity.status(HttpStatus.OK).body(acceptedRide);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<RideDto> getRide(@PathVariable ObjectId id) {
-        RideDto ride = rideService.getRide(id);
+        RideDto ride = rideDriverService.getRide(driverId, id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ride);
     }
 
-    @PostMapping
-    public ResponseEntity<RideDto> createRide(@RequestBody @Valid RideAddingDto addingDto) {
-        RideDto ride = rideService.createRide(addingDto);
+    @PatchMapping("/{driverId}/rides/{id}/payment-status")
+    public ResponseEntity<RideDto> changeRidePaymentStatus(
+            @PathVariable UUID driverId,
+            @PathVariable ObjectId id,
+            @RequestBody @NotNull PaymentStatus paymentStatus) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ride);
+        RideDto ride = ridePaymentService.changeRidePaymentStatus(driverId, id, paymentStatus);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ride);
+    }
+
+    @PatchMapping("/{driverId}/rides/{id}/status")
+    public ResponseEntity<RideDto> changeRideStatus(
+            @PathVariable UUID driverId,
+            @PathVariable ObjectId id,
+            @RequestBody RideStatus status) {
+
+        RideDto ride = rideDriverService.changeRideStatus(driverId, id, status);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ride);
     }
 }
--- ride-service/src/main/java/com/cabaggregator/rideservice/controller/api/RidePassengerController.java
+++ ride-service/src/main/java/com/cabaggregator/rideservice/controller/api/RidePassengerController.java
@@ -2,12 +2,12 @@ package com.cabaggregator.rideservice.controller.api;
 
 import com.cabaggregator.rideservice.core.constant.ValidationErrors;
 import com.cabaggregator.rideservice.core.dto.page.PageDto;
-import com.cabaggregator.rideservice.core.dto.ride.RideAddingDto;
 import com.cabaggregator.rideservice.core.dto.ride.RideDto;
+import com.cabaggregator.rideservice.core.dto.ride.RideUpdatingDto;
 import com.cabaggregator.rideservice.core.enums.RideStatus;
 import com.cabaggregator.rideservice.core.enums.sort.RideSortField;
+import com.cabaggregator.rideservice.service.RidePassengerService;
 import com.cabaggregator.rideservice.service.RidePaymentService;
-import com.cabaggregator.rideservice.service.RideService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
@@ -19,26 +19,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/rides")
-public class RideController {
+@RequestMapping("api/v1/passengers")
+public class RidePassengerController {
 
-    private final RideService rideService;
+    private final RidePassengerService ridePassengerService;
 
     private final RidePaymentService ridePaymentService;
 
-    @GetMapping
-    public ResponseEntity<PageDto<RideDto>> getPageOfRides(
+    @GetMapping("/{passengerId}/rides")
+    public ResponseEntity<PageDto<RideDto>> getPageOfPassengerRides(
+            @PathVariable UUID passengerId,
             @RequestParam(defaultValue = "0")
             @PositiveOrZero(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE_OR_ZERO) Integer offset,
             @RequestParam(defaultValue = "10")
@@ -48,47 +51,41 @@ public class RideController {
             @RequestParam(defaultValue = "ASC") Sort.Direction sortOrder,
             @RequestParam(required = false) RideStatus status) {
 
-        PageDto<RideDto> rides = rideService.getPageOfRides(offset, limit, sortBy, sortOrder, status);
+        PageDto<RideDto> driverRides =
+                ridePassengerService.getPageOfPassengerRides(passengerId, offset, limit, sortBy, sortOrder, status);
 
-        return ResponseEntity.status(HttpStatus.OK).body(rides);
+        return ResponseEntity.status(HttpStatus.OK).body(driverRides);
     }
 
-    @GetMapping("/requests")
-    public ResponseEntity<PageDto<RideDto>> getRequestedRides(
-            @RequestParam(defaultValue = "0")
-            @PositiveOrZero(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE_OR_ZERO) Integer offset,
-            @RequestParam(defaultValue = "10")
-            @Positive(message = ValidationErrors.NUMBER_IS_NOT_POSITIVE)
-            @Max(value = 40, message = ValidationErrors.INVALID_NUMBER_MAX_VALUE) Integer limit,
-            @RequestParam(defaultValue = "id") RideSortField sortBy,
-            @RequestParam(defaultValue = "ASC") Sort.Direction sortOrder) {
-
-        PageDto<RideDto> availableRides =
-                rideService.getPageOfRequestedRides(offset, limit, sortBy, sortOrder);
-
-        return ResponseEntity.status(HttpStatus.OK).body(availableRides);
-    }
-
-    @PutMapping("/rides/requests/{id}")
-    public ResponseEntity<RideDto> acceptRide(
+    @GetMapping("/{passengerId}/rides/{id}")
+    public ResponseEntity<RideDto> getRide(
+            @PathVariable UUID passengerId,
             @PathVariable ObjectId id) {
 
-        RideDto acceptedRide = rideService.acceptRide(id);
-
-        return ResponseEntity.status(HttpStatus.OK).body(acceptedRide);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<RideDto> getRide(@PathVariable ObjectId id) {
-        RideDto ride = rideService.getRide(id);
+        RideDto ride = ridePassengerService.getRide(passengerId, id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ride);
     }
 
-    @PostMapping
-    public ResponseEntity<RideDto> createRide(@RequestBody @Valid RideAddingDto addingDto) {
-        RideDto ride = rideService.createRide(addingDto);
+    @PutMapping("/{passengerId}/rides/{id}")
+    public ResponseEntity<RideDto> updateRide(
+            @PathVariable UUID passengerId,
+            @PathVariable ObjectId id,
+            @RequestBody @Valid RideUpdatingDto updatingDto) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ride);
+        RideDto ride = ridePassengerService.updateRide(passengerId, id, updatingDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ride);
+    }
+
+    @PatchMapping("/{passengerId}/rides/{id}/status")
+    public ResponseEntity<RideDto> changeRideStatus(
+            @PathVariable UUID passengerId,
+            @PathVariable ObjectId id,
+            @RequestBody RideStatus status) {
+
+        RideDto ride = ridePassengerService.changeRideStatus(passengerId, id, status);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ride);
     }
 }
